b3e8e8ce9db467fb014c80914458e1dafc34c7da
lofyer <lofyer@gmail.com>
2016-12-29 12:32:36 +0800

    db: update structure

--- cut-from-db.py
+++ cut-from-db.py
@@ -22,16 +22,20 @@ db = MySQLdb.connect("localhost",
 cursor = db.cursor()
 cursor.execute("show columns from posts")
 data = cursor.fetchall()
+
+# show columns
 print(data)
 
 #
-# Processing data here
+# Process data here
 #
 
-cursor.execute("select id,title,content,url,date_format(str_to_date(date, '%d-%M-%Y'), '%Y%m%d') as date_new from posts where id>1300000;")
+cursor.execute("select id,title,content,url,date_format(str_to_date(date, '%d-%M-%Y'), '%Y%m%d') as date_new from posts where id=1390682;;")
 data = cursor.fetchall()
 for d in data:
-    #print(d[1])
-    cuttest(d[1])
+    print(d[1])
+    #cuttest(d[1])
+    sql = "update rss_rating (id) values %d where id=%d" % d[0]
+    cursor.execute(sql)
 
 db.close()
