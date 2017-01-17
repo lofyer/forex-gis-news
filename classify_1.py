6310ea06ec2a1e9d83897ba299051b051f1547a1
lofyer <lofyer@gmail.com>
2017-01-17 10:44:54 +0800

    GIS update

--- classify_1.py
+++ classify_1.py
@@ -2,15 +2,16 @@
 # -*- coding: UTF-8 -*-
 
 from __future__ import print_function, unicode_literals
-import MySQLdb
+from datetime import date
 import jieba
 import sys
+import MySQLdb
 
 jieba.enable_parallel(4)
 jieba.load_userdict("./jieba/extra_dict/dict.txt.big")
 
 def cuttest(test_sent):
-    result = jieba.cut(test_sent, cut_all=True)
+    result = jieba.cut(test_sent, cut_all=False)
     for word in result:
         print(word, "/", end=' ') 
     print("")
@@ -30,13 +31,26 @@ print(data)
 # Process data here
 #
 
-cursor.execute("select id,title,content,url,date_format(str_to_date(date, '%d-%M-%Y'), '%Y%m%d') as date_new from posts where id=1390682;;")
+#today = date.strftime(date.today(),'%Y%m%d')
+today = "20160711"
+date_format1 = "%d-%M-%Y"
+date_format2 = "%Y%m%d"
+sql_cmd = 'select id,title,content,url,date from posts where date_format(str_to_date(date, "%s"), "%s") like "%s";' % (date_format1, date_format2, today)
+print(sql_cmd)
+cursor.execute(sql_cmd)
 data = cursor.fetchall()
+
+#
+# STEP 1: which country?
+#
 for d in data:
+    if d == "":
+        continue
+    print(d[0])
     print(d[1])
     cuttest(d[1])
-    sql = "update rss_rating (id) values %d where id=%d" % d[0]
-    cursor.execute(sql)
+    #sql = "update rss_rating (id) values %d where id=%d" % (d[0], d[0])
+    #cursor.execute(sql)
 
-db.commit()
+#db.commit()
 db.close()
