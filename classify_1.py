21084a1c36fff91ee6a92ba2cd316ea5c8319022
lofyer <lofyer@gmail.com>
2017-02-08 16:55:47 +0800

    News classify

--- classify_1.py
+++ classify_1.py
@@ -36,34 +36,46 @@ countries_all = cursor.fetchall()
 #
 
 # Get news
-sql_cmd = 'select id,title,content,url,date from posts where id < 200'
+sql_cmd = 'select id,title,content,url,date,content_hash from posts where id > 50000 and id < 55000'
 print(sql_cmd)
 cursor.execute(sql_cmd)
 data = cursor.fetchall()
 
 #
-# STEP 1: which country?
+# STEP 1: which country
 #
+nplaced = 0.0
+nnews = len(data)
 for d in data:
     if d == "":
         continue
-    print(d[0])
-    print(d[1])
-    title = jieba.cut((d[1]), cut_all=False)
-    for t in title:
+    print("ID: %d" % d[0])
+    #print("Title: %s" % d[1])
+    title_word_list = jieba.cut((d[1]), cut_all=False)
+    content_word_list = jieba.cut((d[2]), cut_all=False)
+    placed = False
+    # from title
+    for t in title_word_list:
         for c in countries_all:
             if t == c[0]:
+                nplaced += 1
+                placed = True
                 print(t)
-                print("yes")
-                print("yes")
-                print("yes")
-                print("yes")
-                print("yes")
-                print("yes")
-                print("yes")
-                print("yes")
-    #sql = "update rss_rating (id) values %d where id=%d" % (d[0], d[0])
+    # from content
+    if placed == False:
+        for t in content_word_list:
+            for c in countries_all:
+                if t == c[0]:
+                    nplaced += 1
+                    placed = True
+                    print(t)
+
+    #sql = "update rss_rating (content_hash) values %d where content_hash=%s" % (d[5], d[5])
     #cursor.execute(sql)
+
+print(nplaced)
+print(nnews)
+print('Percent of placed: %f' % (nplaced/nnews))
 
 #db.commit()
 db.close()
