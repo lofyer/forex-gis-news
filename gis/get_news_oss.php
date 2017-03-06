d6129107bc4066e65b7f2d968285a70dc02d5ba3
root <root@ubuntu.members.linode.com>
2017-03-06 13:04:32 +0800

    update

--- gis/get_news_oss.php
+++ gis/get_news_oss.php
@@ -15,28 +15,34 @@ $request->index('db_crawl')
         ->operator(OpenSearchServer\Search\Search::OPERATOR_OR)
         ->emptyReturnsAll()
         ->rows(9999)
-        ->sort('date', OpenSearchServer\Search\Search::SORT_DESC)
+        ->sort('date_news', OpenSearchServer\Search\Search::SORT_DESC)
+        ->sort('title', OpenSearchServer\Search\Search::SORT_DESC)
         //->searchField('title')
         //->searchField('content')
         ->filterField('date', "[$yesterday TO $cur_date]")
         ->returnedFields('title')
         ->returnedFields('date')
+        ->returnedFields('date_news')
         ->returnedFields('url')
         ->returnedFields('content')
         ->returnedFields('content_hash');
 $results = $oss_api->submit($request);
 
-echo "News of $cur_date</br>";
+echo "News from $yesterday to $cur_date</br>";
 echo 'Total number of results: ' . $results->getTotalNumberFound() . '<br/>';
 echo 'Number of results in this set of results: ' . $results->getNumberOfResults();
 
 foreach($results as $key => $result) {
     //echo '<hr/>Result #'.$key.': <br/>';
     echo '<hr/>';
-    echo "<h2><a href=$result->getField('url') target=_blank>".$result->getField('title').'</a></h2></br>';
+    echo "<h2><a href=".$result->getField("url"). " target=_blank>" .$result->getField('title').'</a></h2>';
+    echo "<em>";
+    echo $result->getField('content_hash');
+    echo "</br>";
     echo $result->getField('date');
     echo "</br>";
-    echo $result->getField('content_hash');
+    echo $result->getField('date_news');
+    echo "</em>";
     echo "</br>";
     //echo "</br><a href=$result->getField('url') target=_blank>".$result->getField('url').'</a></br>';
     echo $result->getField('content');
